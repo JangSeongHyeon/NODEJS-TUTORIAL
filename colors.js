f1516d133db3d8ec053b1d7c8f572c1d1f6e4aa2
JangSeongHyeon <dwg04045@gmail.com>
2021-01-11 18:38:23 +0900

    update javascript to jquery

--- colors.js
+++ colors.js
@@ -1,18 +1,21 @@
 var Links={
     setColor:function(color){
-        var list=document.querySelectorAll('a');
-        for(var i=0;i<list.length;i++){
-            list[i].style.color=color
-        }
+        // var list=document.querySelectorAll('a');
+        // for(var i=0;i<list.length;i++){
+        //     list[i].style.color=color
+        // }
+        $('a').css("color",color);
     }
 }
 
 var Body={
     setColor: function(color){
-        document.querySelector('body').style.color=color;
+        //document.querySelector('body').style.color=color;
+        $('body').css("color",color);
     },
     SetBackgroundColor:function(color){
-        document.querySelector('body').style.backgroundColor=color;
+        // document.querySelector('body').style.backgroundColor=color;
+        $('body').css("backgroundColor",color)
     }
 }
 
